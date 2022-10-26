34196a9567646f8e9934e9c202bc06c26edb0a75
Destiny Boone <destaj.db@gmail.com>
2022-10-26 15:58:15 -0400

    directives assignment completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,33 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: [`
+    .whiteColor{
+      color: #fff;
+    }
+  `]
 })
 export class AppComponent {
+  isClicked = false;
+  buttonlabel = 'Display Details';
+  counter = 0;
+  clicks = [];
+
+  revealSecret(){
+    this.counter ++;
+    this.clicks.push(this.counter);
+
+    if(this.isClicked === false){
+      this.isClicked = true;
+      this.buttonlabel = 'Hide Details';
+    }
+    else{
+      this.isClicked = false;
+      this.buttonlabel = 'Display Details';
+    }
+  }
+
+  getBackgroundColor(click){
+    return click >= 5 ? 'blue' : 'transparent';
+  }
 }
